740721ad46fb8bf9c272184c55c607a867917642
Sanjib Kumar Sen <sksenonline@gmail.com>
2025-03-23 23:11:46 +0600

    sidebar done

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,14 +1,15 @@
 import { Box, Flex, Icon, Text } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link as RouterLink } from "@tanstack/react-router"
-import { FiBriefcase, FiHome, FiSettings, FiUsers } from "react-icons/fi"
+import { FiBriefcase, FiHome, FiSettings, FiUsers, FiClock, FiBookmark } from "react-icons/fi"
 import type { IconType } from "react-icons/lib"
 
 import type { UserPublic } from "@/client"
 
 const items = [
-  { icon: FiHome, title: "Dashboard", path: "/" },
-  { icon: FiBriefcase, title: "Items", path: "/items" },
+  { icon: FiHome, title: "Search", path: "/" },
+  { icon: FiClock, title: "Search history", path: "/history" },
+  { icon: FiBookmark, title: "Bookmarks", path: "/bookmarks" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
 
